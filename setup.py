112fe2ea0d5c5d6b399fbed833370d3221f64c6c
Philipp Ploder <philipp.ploder@outlook.com>
2018-03-08 23:40:14 +0100

    Bump version and change readme to RST

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
+from os import path
 from setuptools import setup
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='nice_list',
-    version='1.0.0',
+    version='1.1.0',
 
     description='Pretty prints Python lists',
+    long_description=long_description,
     author='Philipp Ploder',
     url='https://github.com/Fylipp/nice-list',
 
